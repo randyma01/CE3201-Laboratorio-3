7160ecd649afc6244cc24c1c7cfd61866ae04e5c
kvn-95 <kevin.gonzalez0795@outlook.com>
2021-08-31 20:02:42 -0600

    Modulos de operaciones de la ALU pt1

---
+++ experimento-1/and_module.sv
@@ -0,0 +1,17 @@
+module and_module #(parameter N=4)(
+	input logic [N-1:0] a,
+	input logic [N-1:0] b,
+	output logic [N-1:0] result,
+	output logic [N-1:0] flags
+);
+
+	assign result = a && b;
+	always @(result)
+	begin
+		if(result == 4'b0000)
+			flags=4'b0001;
+		else
+			flags=4'b0000;
+	
+	end
+endmodule
