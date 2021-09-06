57fb999e2d42f5dfa10b5b1371463cc1b10b920c
kvn-95 <kevin.gonzalez0795@outlook.com>
2021-09-06 13:56:48 -0600

    Update al sumador y restador para que se ejecuten de manera dinamica segun cant. de bits

--- experimento-1/nBitSubstractor_module.sv
+++ experimento-1/nBitSubstractor_module.sv
@@ -5,35 +5,35 @@ module nBitSubstractor_module #(parameter N=4)
   output logic [N-1:0] result,
   output logic [3:0] flags
  );
+
+  logic co;
+  logic [N-1:0] c;
+  genvar i;
+ 
+  generate
+    
+	 assign co = 0;
+	 
+	 oneBitFullSubstractor_module oba0 (a[0], b[0], co, result[0], c[0]);
+	 
+	 for(i=1; i<N; i=i+1) begin: forloop
+	  oneBitFullSubstractor_module obaN (a[i],b[i],c[i-1],result[i],c[i]);	 
+	 end
+	
+	 always @(result)
+	   begin
+		  if(result == 4'b0000 && c[N-1]==0)
+		    flags=4'b0001;
+		  else if((c[N-1]==1))
+			 flags=4'b0100;
+		  else
+			 flags=4'b0000;
+	   end		
+	 
+  endgenerate 
+ 
  
 	/*
-
-	logic co = 0;
-	logic [N-1:0] c;
-	genvar i;
-	
-	oneBitFullAdder_module oba0 (a[0],b[0],co,result[0],c[0]);
-	
-	generate forBlock
-		for (i=1; i < N; i = i+1)
-		begin
-			oneBitFullAdder_module obaN (a[i],b[i],c[i-1],result[i],c[i]);	
-		end
-		
-	endgenerate
-	
-	always @(result)
-    begin
-      if(result == 4'b0000 && c4==0)
-	     flags=4'b0001;
-		else if((result == 4'b0000 && c4==1) || (c4==1))
-			flags=4'b0010;
-	   else
-	     flags=4'b0000;
-    end
-	 
-	 */
-
  
  	logic co = 0;
 	logic c1,c2,c3,c4;
@@ -53,5 +53,7 @@ module nBitSubstractor_module #(parameter N=4)
 	   else
 	     flags=4'b0000;
     end
+	 
+	 */
 	
 endmodule 
--- experimento-1/nBitSubstractor_module_tb.sv
+++ experimento-1/nBitSubstractor_module_tb.sv
@@ -1,28 +1,27 @@
 module nBitSubstractor_module_tb #(parameter N=4)();
 	
-	logic [N-1:0] a;
-	logic [N-1:0] b;
-	logic [N-1:0] r;
-	logic [3:0] f;
-	nBitSubstractor_module modulo(a,b,r,f);
+  logic [N-1:0] a;
+  logic [N-1:0] b;
+  logic [N-1:0] r;
+  logic [3:0] f;
+  nBitSubstractor_module modulo(a,b,r,f);
 	
-	initial begin
-	$display("Iniciando simulacion");
-	a=4'b1111;
-	b=4'b0001;
-	#40
+  initial begin
+    $display("Iniciando simulacion");
+	  a=4'b1001;
+	  b=4'b0101;
+	  #40
 	
-	a=4'b0001;
-	b=4'b0001;
-	#40
+	  a=4'b0001;
+	  b=4'b0001;
+	  #40
 	
-	a=4'b1000;
-	b=4'b0100;
-	#40
+	  a=4'b1000;
+	  b=4'b0100;
+	  #40
 	
-	a=4'b0001;
-	b=4'b0011;
+	  a=4'b0001;
+	  b=4'b0011;
 	
-	
-	end
-endmodule
+  end
+endmodule 
